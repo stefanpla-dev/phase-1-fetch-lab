164406246fcd16ec6788f8a6fb654fc98a1224c7
Stefan Pla <spla@icloud.com>
2024-02-12 16:15:38 -0500

    made small change to see if I get a passing grade this time, all tests pass

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then((books) => {
+      renderBooks(books);
+    });
+  };
 
 function renderBooks(books) {
+  // console.log(books);
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
